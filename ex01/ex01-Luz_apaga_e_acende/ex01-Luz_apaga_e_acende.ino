57ddf98595eb5539d0648f9d5968c957fa87dc69
ivansftdejc <ivandroadelino98@gmail.com>
2026-05-16 22:26:37 +0100

    Exercicio LED pin 13

--- ex01/ex01-Luz_apaga_e_acende/ex01-Luz_apaga_e_acende.ino
+++ ex01/ex01-Luz_apaga_e_acende/ex01-Luz_apaga_e_acende.ino
@@ -6,17 +6,11 @@ pinMode(13, OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
  digitalWrite(13, HIGH);
- delay(1000);
+ delay(250);
  digitalWrite(13, LOW);
- delay(500);
-  digitalWrite(13, HIGH);
- delay(1000);
- digitalWrite(13, LOW);
- delay(500);
-  digitalWrite(13, HIGH);
- delay(1000);
- digitalWrite(13, LOW);
- delay(500);
+ delay(250);
  digitalWrite(13, HIGH);
- delay(3000);
+ delay(250);
+ digitalWrite(13, LOW);
+ delay(2000);
 }
